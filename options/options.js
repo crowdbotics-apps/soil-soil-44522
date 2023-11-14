2fa18c010fb5743d8b8a6c21889546e7a2c96226
Crowdbotics <team@crowdbotics.com>
2023-11-14 18:15:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soil-soil-44522",
+  url: "https://soil-soil-44522.botics.co",
+  api: "https://soil-soil-44522.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
